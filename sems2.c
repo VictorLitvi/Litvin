1a13a1d3ff332c837940eb0c2fd579e659e13ece
Litvinow <vikt.litwinow2018@yandex.ru>
2024-05-31 11:28:48 +0700

    Добавили в программы, работающие с семафорами, комментарии

--- sems2.c
+++ sems2.c
@@ -5,17 +5,21 @@
 #include <stdlib.h>
 
 int main () {
-	int semid;
-	char pathname [] = "for_ipc.ipc";
-	key_t key;
-	struct sembuf mybuf;
-	if ((key = ftok(pathname, 0 )) < 0) {
-		printf ("Не удалось сгенерировать ключ ipc\n");
-		exit (-1) ;
-	}
-	if ((semid = semget (key, 1 , 0666 | IPC_CREAT)) <0 ) {
-		printf ("Не удалось получить дескриптор массива семафоров\n");	
-		exit (-1);
+	int semid;//IPC - дескриптор для массива семафоров 
+        char pathname [] = "for_ipc.ipc"//Имя файла, с помощью которого генерируется ipc-ключ
+        key_t key;// Переменная для хранения ipc-ключа
+        struct sembuf mybuf;//Структура данных, задающая операцмм над определенным семафором
+        //Процесс генерации ipc-ключа
+        if ((key = ftok(pathname, 0 )) < 0) {
+                printf ("Не удалось сгенерировать ключ ipc\n");
+                exit (-1) ;
+        }
+        //Процесс полученния доступа к массиву семафоров для конкретного  ipc-ключа
+        //Если массива семафоров не существует, то пытаемся его создать 
+        if ((semid = semget (key, 1 , 0666 | IPC_CREAT)) <0 ) {
+                printf ("Не удалось получить дескриптор массива семафоров\n");  
+                exit (-1);
+
 	}
 	mybuf.sem_op = 1;
 	mybuf.sem_flg = 0;
